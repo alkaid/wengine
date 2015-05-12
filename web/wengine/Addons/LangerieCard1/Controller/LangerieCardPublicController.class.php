53adea585033cd518faa4f3eb4640bcf027f43ed
alkaid <muliangcong@gmail.com>
2015-05-12 18:36:25 +0800

    DD

---
+++ web/wengine/Addons/LangerieCard1/Controller/LangerieCardPublicController.class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Addons\LangerieCard1\Controller;
+use Home\Controller\AddonsController;
+
+class LangerieCardPublicController extends AddonsController{
+    function consume(){
+
+    }
+
+    function detail(){
+        //TODO 应该做成素材管理模块 get参数获得素材id 引用素材页面再插入所需js
+
+    }
+}
---
+++ web/wengine/Addons/LangerieCard1/Model/LangerieCard1Model.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Addons\LangerieCard1\Model;
+use Think\Model;
+
+/**
+ * LangerieCard1模型
+ */
+class LangerieCard1Model extends Model{
+
+}
